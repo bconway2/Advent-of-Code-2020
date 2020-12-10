029ebd27a4bd53625eab76f2ceb1c24986d033f7
bconway2 <75540797+bconway2@users.noreply.github.com>
2020-12-10 09:37:56 -0500

    Update day10.py

--- day10/day10.py
+++ day10/day10.py
@@ -21,12 +21,39 @@ def part_one(day_input):
 def part_two(day_input):
     last = 0
     must_use = []
-    ''' Need to have any adapters where diff is 3'''
+
+    '''Must use any adapters where nearest diff is 3'''
     for i in day_input:
         test = i - last
         if test == 3:
             must_use.append(last)
             must_use.append(i)
         last = i
-    must_use = list(set(must_use)).sort()
-    return must_use
+    
+    '''remove dups'''
+    mand_adpts = []
+    for i in must_use:
+        if i not in mand_adpts:
+            mand_adpts.append(i)
+
+    '''determine dof between mandatory adapters'''
+    last = 0
+    tricky_cases = []
+    combinations = 1
+    for adpt in mand_adpts:
+        diff = adpt - last
+        opt_adpt = [x for x in day_input if last < x < adpt]
+        if diff <= 3: # any adapters in this range are optional
+            combinations *= 2 ** len(opt_adpt)
+        elif diff == 4: # need to have at least one of the included adapters
+            combinations *= (2 ** len(opt_adpt) - 1)
+        else: # fortunately the gap doesn't get larger because the logic would
+            # have quickly gotten very very messy
+            tricky_cases.append([x for x in day_input if last < x < adpt])
+        last = adpt
+    return combinations
+
+day10inp = get_input('day10input.txt')
+
+print(part_one(day10inp))
+print(part_two(day10inp))
